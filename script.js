5e6963a8acc68eb7f879872d386bf5adfa6d9242
DESKTOP-JQELIK3\JOMUAM <present_08@naver.com>
2020-07-07 21:52:25 +0900

    Final Touching

--- script.js
+++ script.js
@@ -2,58 +2,59 @@
 
 // scrollY navbar
 document.addEventListener('scroll', () => {
-    const navbar = document.querySelector('#navbar');
-    const navbarHeight = navbar.getBoundingClientRect().height;
+  const navbar = document.querySelector('#navbar');
+  const navbarHeight = navbar.getBoundingClientRect().height;
 
-    if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
 });
 
 // reponsive navbar handler
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarToggleBtn.addEventListener('click', () => {
-    navbarMenu.classList.toggle("on");
+  navbarMenu.classList.toggle('on');
 });
 
 // hendle scrolling navbar menu
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-        return;
-    };
-    navbarMenu.classList.remove('on');
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
-})
-
-
-
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('on');
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
 
 //project
-const workBtnContainer = document.querySelector(".work__btn");
-const projectContainer = document.querySelector(".work__project");
+const workBtnContainer = document.querySelector('.work__btn');
+const projectContainer = document.querySelector('.work__project');
 const projects = document.querySelectorAll('.project__list.main');
 
-workBtnContainer.addEventListener("click", (e) => {
-    const filter = e.target.dataset.filter;
-    if (filter == null) {
-        return;
-    }
-    projectContainer.classList.add('anim-out');
-    setTimeout(() => {
-        projectContainer.classList.remove('anim-out');
-        projects.forEach((project) => {
-            if (filter === '*' || filter === project.dataset.type) {
-                project.classList.remove('invisible');
-            } else {
-                project.classList.add('invisible');
-            }
-        });
-    }, 300);
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  //click project Btn
+  const active = document.querySelector('.work__menu.active');
+  active.classList.remove('active');
+  e.target.classList.add('active');
 
-})
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectContainer.classList.remove('anim-out');
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+  }, 300);
+});
